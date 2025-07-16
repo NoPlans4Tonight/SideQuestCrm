044ee9ce176346a5088dfd1ee55a70ef8858bc9c
NoPlans4Tonight <jlochan53@gmail.com>
2025-07-15 23:48:59 -0500

    refactor: update web routes to SPA catch-all for Vue router
    - Replace individual route definitions with a catch-all route for SPA
    - Ensures all navigation is handled by Vue Router

--- routes/web.php
+++ routes/web.php
@@ -2,48 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Catch-all route for Vue.js SPA
+Route::get('/{any}', function () {
     return view('app');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('app');
-    })->name('dashboard');
-
-    Route::get('/customers', function () {
-        return view('app');
-    })->name('customers.index');
-
-    Route::get('/customers/create', function () {
-        return view('app');
-    })->name('customers.create');
-
-    Route::get('/customers/{id}', function () {
-        return view('app');
-    })->name('customers.show');
-
-    Route::get('/customers/{id}/edit', function () {
-        return view('app');
-    })->name('customers.edit');
-
-    Route::get('/jobs', function () {
-        return view('app');
-    })->name('jobs.index');
-
-    Route::get('/jobs/create', function () {
-        return view('app');
-    })->name('jobs.create');
-
-    Route::get('/jobs/{id}', function () {
-        return view('app');
-    })->name('jobs.show');
-
-    Route::get('/jobs/{id}/edit', function () {
-        return view('app');
-    })->name('jobs.edit');
-});
+})->where('any', '.*');
