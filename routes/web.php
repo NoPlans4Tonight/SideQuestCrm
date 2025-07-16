11fbbeeaabc27696ae3ce65038d54717e1183733
NoPlans4Tonight <jlochan53@gmail.com>
2025-07-15 23:39:06 -0500

    feat: update routing and templates for Vue.js SPA
    - Update web routes to serve single app.blade.php view
    - Create app.blade.php template to load Vue.js application
    - Add button-link component for navigation
    - Configure SPA routing for Vue.js frontend

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
 Route::middleware([
@@ -12,6 +12,38 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('app');
     })->name('dashboard');
+
+    Route::get('/customers', function () {
+        return view('app');
+    })->name('customers.index');
+
+    Route::get('/customers/create', function () {
+        return view('app');
+    })->name('customers.create');
+
+    Route::get('/customers/{id}', function () {
+        return view('app');
+    })->name('customers.show');
+
+    Route::get('/customers/{id}/edit', function () {
+        return view('app');
+    })->name('customers.edit');
+
+    Route::get('/jobs', function () {
+        return view('app');
+    })->name('jobs.index');
+
+    Route::get('/jobs/create', function () {
+        return view('app');
+    })->name('jobs.create');
+
+    Route::get('/jobs/{id}', function () {
+        return view('app');
+    })->name('jobs.show');
+
+    Route::get('/jobs/{id}/edit', function () {
+        return view('app');
+    })->name('jobs.edit');
 });
