f97047dfeb97fc19a99c5405d7ce3616b9781f63
NoPlans4Tonight <jlochan53@gmail.com>
2025-07-16 19:05:25 -0500

    test: add factory for Tenant model to support testing

---
+++ database/factories/TenantFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tenant>
+ */
+class TenantFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tenant::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'domain' => $this->faker->domainName(),
+            'database' => 'tenant_' . $this->faker->unique()->numberBetween(1000, 9999),
+            'company_name' => $this->faker->company(),
+            'contact_email' => $this->faker->companyEmail(),
+            'contact_phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'zip_code' => $this->faker->postcode(),
+            'country' => 'US',
+            'timezone' => 'UTC',
+            'settings' => [
+                'timezone' => $this->faker->timezone(),
+                'currency' => 'USD',
+                'date_format' => 'Y-m-d',
+                'time_format' => 'H:i:s',
+            ],
+            'is_active' => true,
+        ];
+    }
+
+    /**
+     * Indicate that the tenant is inactive.
+     */
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_active' => false,
+        ]);
+    }
+}
